16eee5b1eff863878d0fd90a0d75db7eec4c5a6a
oncore <oncore@oncoreubuntu>
2016-06-01 12:57:03 -0700

    [#120376613] adding initial selenium smoketest class and suporting jars

---
+++ OncoreCHHS-selenium/src/com/oncore/chhs/web/selenium/SeleniumLoginTest.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 oncore.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.oncore.chhs.web.selenium;
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
+
+public class SeleniumLoginTest {
+	private Selenium selenium;
+
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://oncorechhsjenkins.westus.cloudapp.azure.com:8080/OncoreCHHS-war/");
+		selenium.start();
+	}
+
+	@Test
+	public void testSeleniumLogin() throws Exception {
+		selenium.open("/OncoreCHHS-war/");
+		selenium.click("link=Login");
+		selenium.waitForPageToLoad("30000");
+		selenium.type("id=chssForm:userNameTxt:input", "testuser");
+		selenium.click("id=chssForm:loginBtn");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
