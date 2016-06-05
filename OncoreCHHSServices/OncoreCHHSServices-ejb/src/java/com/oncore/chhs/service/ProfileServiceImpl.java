c94bb9456ff4d19ad308de3cb4f0549a8d56f1cc
mhannaford-oncore <michael.hannaford@oncorellc.com>
2016-06-04 21:20:41 -0700

    [#120374849] Refactored Profile Service to use new DTO converter

--- OncoreCHHSServices/OncoreCHHSServices-ejb/src/java/com/oncore/chhs/service/ProfileServiceImpl.java
+++ OncoreCHHSServices/OncoreCHHSServices-ejb/src/java/com/oncore/chhs/service/ProfileServiceImpl.java
@@ -26,15 +26,13 @@ package com.oncore.chhs.service;
 import com.oncore.chhs.client.dto.profile.Profile;
 import com.oncore.chhs.client.ejb.ProfileService;
 import com.oncore.chhs.persistence.dao.UserDAO;
-import com.oncore.chhs.persistence.entity.Address;
-import com.oncore.chhs.persistence.entity.Contact;
 import com.oncore.chhs.persistence.entity.Users;
+import com.oncore.chhs.service.converter.UsersToProfileDTOConverter;
 import java.util.Arrays;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
-import org.apache.commons.collections4.CollectionUtils;
 
 /**
  *
@@ -44,9 +42,6 @@ import org.apache.commons.collections4.CollectionUtils;
 @Remote(ProfileService.class)
 public class ProfileServiceImpl implements ProfileService {
 
-    private static final List<String> CONTACT_PHONE_TYPES
-            = Arrays.asList("HPH", "MPH", "SMS", "WPH");
-
     @EJB
     private UserDAO userDAO;
 
@@ -55,36 +50,7 @@ public class ProfileServiceImpl implements ProfileService {
 
         Users users = this.userDAO.findById(userUid);
 
-        Profile profile = null;
-
-        if (users != null) {
-            profile = new Profile();
-            
-            profile.setUserName(users.getUsrUserId());
-            profile.setFirstName(users.getUsrFirstname());
-            profile.setMiddleName(users.getUsrMiddlename());
-            profile.setLastName(users.getUsrLastname());
-
-            if (CollectionUtils.isNotEmpty(users.getAddressSet())) {
-                Address address = users.getAddressSet().iterator().next();
-
-                profile.setAddressLine1(address.getAdrLine1());
-                profile.setAddressLine2(address.getAdrLine2());
-                profile.setCity(address.getAdrCity());
-                profile.setState(address.getAdrStateCd().getCode());
-                profile.setZip(address.getAdrZip5() + address.getAdrZip4());
-            }
-
-            if (CollectionUtils.isNotEmpty(users.getContactSet())) {
-                for (Contact contact : users.getContactSet()) {
-                    if (CONTACT_PHONE_TYPES.contains(contact.getEmcTypeCd().getCode())) {
-                        profile.setPhone(contact.getEmcValue());
-                    } else {
-                        profile.setEmail(contact.getEmcValue());
-                    }
-                }
-            }
-        }
+        Profile profile = UsersToProfileDTOConverter.convert(users);
 
         return profile;
     }
