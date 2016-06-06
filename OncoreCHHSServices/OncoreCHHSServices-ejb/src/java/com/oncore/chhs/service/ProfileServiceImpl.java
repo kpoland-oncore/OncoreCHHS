baa03e53f0bee6f6811a1c598239b0bdb5c1d61e
mhannaford-oncore <michael.hannaford@oncorellc.com>
2016-06-06 10:34:09 -0700

    [#120374849] Finished UpdateProfile function migration to RESTful web services.

--- OncoreCHHSServices/OncoreCHHSServices-ejb/src/java/com/oncore/chhs/service/ProfileServiceImpl.java
+++ OncoreCHHSServices/OncoreCHHSServices-ejb/src/java/com/oncore/chhs/service/ProfileServiceImpl.java
@@ -39,12 +39,14 @@ import com.oncore.chhs.persistence.entity.Users;
 import com.oncore.chhs.service.converter.UsersToProfileDTOConverter;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Set;
 import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import org.apache.commons.lang3.StringUtils;
 
 /**
+ * Implementation class for the ProfileService.
  *
  * @author oncore
  */
@@ -97,15 +99,11 @@ public class ProfileServiceImpl implements ProfileService {
 
     /**
      * Create a new address for the user.
-     * 
+     *
      * @param users The Users entity, which the new Address will be added to.
      * @param profile The Profile DTO, containing the new address info.
      */
     private void createAddress(Users users, Profile profile) {
-
-        if (users.getAddressSet() == null) {
-            users.setAddressSet(new HashSet<>());
-        }
 
         Address address = new Address();
         address.setUsrUidFk(users);
@@ -131,7 +129,8 @@ public class ProfileServiceImpl implements ProfileService {
         address.setUpdateTs(new Date());
         address.setUpdateUserId(getFormattedName(users));
 
-        users.getAddressSet().add(address);
+        Set<Address> addresses = this.getUserAddresses(users);
+        addresses.add(address);
 
         this.addressDAO.create(address);
     }
@@ -159,22 +158,19 @@ public class ProfileServiceImpl implements ProfileService {
         contact.setUpdateTs(new Date());
         contact.setUpdateUserId(getFormattedName(users));
 
-        users.getContactSet().add(contact);
+        Set<Contact> contacts = this.getUserContacts(users);
+        contacts.add(contact);
 
         this.contactDAO.create(contact);
     }
 
     /**
      * Create a new email Contact for the user.
-     * 
+     *
      * @param users The Users entity, which the new Contact will be added to.
      * @param profile The Profile DTO, containing the new email info.
      */
     private void createContactEmail(Users users, Profile profile) {
-
-        if (users.getContactSet() == null) {
-            users.setContactSet(new HashSet<>());
-        }
 
         Contact contact = new Contact();
         contact.setUsrUidFk(users);
@@ -187,14 +183,158 @@ public class ProfileServiceImpl implements ProfileService {
         contact.setUpdateTs(new Date());
         contact.setUpdateUserId(getFormattedName(users));
 
-        users.getContactSet().add(contact);
+        Set<Contact> contacts = this.getUserContacts(users);
+        contacts.add(contact);
 
         this.contactDAO.create(contact);
     }
 
     @Override
     public void updateProfile(CreateOrUpdateProfile profile) {
-        throw new RuntimeException( "Update Profile is not implemented yet");
+
+        Users users = this.userDAO.findById(profile.getUserUid());
+        users.setUsrFirstname(profile.getFirstName());
+        users.setUsrMiddlename(profile.getMiddleName());
+        users.setUsrLastname(profile.getLastName());
+
+        this.updateUsersAddress(profile, users);
+        this.updateUsersEmail(profile, users);
+        this.updateUsersPhone(profile, users);
+
+    }
+
+    /**
+     * Update the user's address in their profile.
+     *
+     * <ul>
+     * <li>If the address is not blank, and the user has an existing address,
+     * update it to the new value.</li>
+     * <li>If the address is not blank, and the user has no existing address,
+     * create one.</li>
+     * <li>If the address is blank, and the user has an existing address, delete
+     * it.</li>
+     * </ul>
+     *
+     * @param profile The updated profile info.
+     * @param users The users entity.
+     */
+    private void updateUsersAddress(CreateOrUpdateProfile profile, Users users) {
+        if (StringUtils.isNotBlank(profile.getAddressLine1())) {
+            Set<Address> addresses = this.getUserAddresses(users);
+            Address address = addresses.iterator().next();
+
+            if (address != null) {
+                address.setAdrLine1(profile.getAddressLine1());
+                address.setAdrLine2(profile.getAddressLine2());
+                address.setAdrCity(profile.getCity());
+
+                AdrStateCd adrStateCd = this.adrStateCdDAO.findById(profile.getState());
+                address.setAdrStateCd(adrStateCd);
+
+                if (StringUtils.isNotBlank(profile.getZip())) {
+                    String newZip = profile.getZip();
+                    if (newZip.length() > 5) {
+                        address.setAdrZip5(newZip.substring(0, 4));
+                        address.setAdrZip4(newZip.substring(5));
+                    } else {
+                        address.setAdrZip5(newZip);
+                        address.setAdrZip4("");
+                    }
+                } else {
+                    address.setAdrZip5("");
+                    address.setAdrZip4("");
+                }
+
+            } else {
+                this.createAddress(users, profile);
+            }
+        } else {
+            Set<Address> addresses = users.getAddressSet();
+            if (addresses != null) {
+                Address address = addresses.iterator().next();
+                if (address != null) {
+                    addresses.remove(address);
+                    this.addressDAO.delete(address);
+                }
+            }
+
+        }
+    }
+
+    /**
+     * Update the user's email address in their profile.
+     *
+     * <ul>
+     * <li>If the email address is not blank, and the user has an existing email
+     * contact, update it to the new value.</li>
+     * <li>If the email address is not blank, and the user has no existing email
+     * contact, create one.</li>
+     * <li>If the email address is blank, and the user has an existing email
+     * contact, delete it.</li>
+     * </ul>
+     *
+     * @param profile The updated profile info.
+     * @param users The users entity.
+     */
+    private void updateUsersEmail(CreateOrUpdateProfile profile, Users users) {
+        if (StringUtils.isNotBlank(profile.getEmail())) {
+            Set<Contact> contacts = this.getUserContacts(users);
+            Contact emailContact = this.findEmailContact(contacts);
+
+            if (emailContact != null) {
+                emailContact.setEmcValue(profile.getEmail());
+            } else {
+                this.createContactEmail(users, profile);
+            }
+        } else {
+            Set<Contact> contacts = users.getContactSet();
+            if (contacts != null) {
+                Contact emailContact = this.findEmailContact(contacts);
+                if (emailContact != null) {
+                    contacts.remove(emailContact);
+                    this.contactDAO.delete(emailContact);
+                }
+            }
+        }
+    }
+
+    /**
+     * Update the user's phone in their profile.
+     *
+     * <ul>
+     * <li>If the phone is not blank, and the user has an existing phone
+     * contact, update it to the new value.</li>
+     * <li>If the phone is not blank, and the user has no existing phone, create
+     * one.</li>
+     * <li>If the phone is blank, and the user has an existing phone, delete
+     * it.</li>
+     * </ul>
+     *
+     * @param profile The updated profile info.
+     * @param users The users entity.
+     */
+    private void updateUsersPhone(CreateOrUpdateProfile profile, Users users) {
+        if (StringUtils.isNotBlank(profile.getPhone())) {
+            Set<Contact> contacts = this.getUserContacts(users);
+            Contact phoneContact = this.findPhoneContact(contacts);
+
+            if (phoneContact != null) {
+                EmcTypeCd emcTypeCd = this.emcTypeCdDAO.findById(profile.getPhoneType());
+                phoneContact.setEmcTypeCd(emcTypeCd);
+                phoneContact.setEmcValue(profile.getPhone());
+            } else {
+                this.createContactPhone(users, profile);
+            }
+        } else {
+            Set<Contact> contacts = users.getContactSet();
+            if (contacts != null) {
+                Contact phoneContact = this.findPhoneContact(contacts);
+                if (phoneContact != null) {
+                    contacts.remove(phoneContact);
+                    this.contactDAO.delete(phoneContact);
+                }
+            }
+        }
     }
 
     /**
@@ -222,6 +362,84 @@ public class ProfileServiceImpl implements ProfileService {
         }
 
         return formattedName;
+    }
+
+    /**
+     * Get the user's set of addresses, if it exists. If it doesn't exist,
+     * create it and add it to the entity.
+     *
+     * @param users The users entity.
+     *
+     * @return The user's addresses.
+     */
+    private Set<Address> getUserAddresses(Users users) {
+        Set<Address> addresses = users.getAddressSet();
+
+        if (addresses == null) {
+            addresses = new HashSet<>();
+            users.setAddressSet(addresses);
+        }
+
+        return addresses;
+    }
+
+    /**
+     * Get the user's set of contacts, if it exists. If it doesn't exist, create
+     * it and add it to the entity.
+     *
+     * @param users The users entity.
+     *
+     * @return The user's contacts.
+     */
+    private Set<Contact> getUserContacts(Users users) {
+        Set<Contact> contacts = users.getContactSet();
+
+        if (contacts == null) {
+            contacts = new HashSet<>();
+            users.setContactSet(contacts);
+        }
+
+        return contacts;
+    }
+
+    /**
+     * Find the email contact in the set.
+     *
+     * @param contacts The user's contact set.
+     *
+     * @return The email contact, or null if none exists.
+     */
+    private Contact findEmailContact(Set<Contact> contacts) {
+        Contact email = null;
+
+        for (Contact contact : contacts) {
+            if ("EML".equals(contact.getEmcTypeCd().getCode())) {
+                email = contact;
+                break;
+            }
+        }
+
+        return email;
+    }
+
+    /**
+     * Find the phone contact in the set.
+     *
+     * @param contacts The user's contact set.
+     *
+     * @return The phone contact, or null if none exists.
+     */
+    private Contact findPhoneContact(Set<Contact> contacts) {
+        Contact phone = null;
+
+        for (Contact contact : contacts) {
+            if (!"EML".equals(contact.getEmcTypeCd().getCode())) {
+                phone = contact;
+                break;
+            }
+        }
+
+        return phone;
     }
 
 }
